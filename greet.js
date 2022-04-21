8a1f78877c0a1f25421a6361a2e7f4dccaa857d8
Yonela <yoza.futho@gmail.com>
2022-04-21 12:47:30 +0200

    TDD testing

---
+++ greet.js
@@ -0,0 +1,9 @@
+function isFromBellville(number){
+    var results = number.startsWith("CY");
+     return results;
+ }
+
+ function isFromCapeTown(number){
+    var results = number.startsWith("CA");
+    return results;
+  }
